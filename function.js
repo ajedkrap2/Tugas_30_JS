9aa50163232e0b87eb1ca6d40138a5de8b0a67e6
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:35:36 +0700

    submit

---
+++ function.js
@@ -0,0 +1,8 @@
+function regex(value){
+  var tugas30 = value.match(/[es]/g);
+  console.log(tugas30);
+}
+
+regex(
+ "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat"
+);
